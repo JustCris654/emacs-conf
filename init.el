7e380dc7054ec3f754cb29306c7d54a5f65f8cc2
Cristian Scapin <cristian.scapin654@icloud.com>
2025-08-12 17:36:03 +0200

    themes

--- init.el
+++ init.el
@@ -8,7 +8,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tsdh-dark)))
+ '(custom-enabled-themes '(tsdh-dark))
+ '(custom-safe-themes
+   '("d2ab3d4f005a9ad4fb789a8f65606c72f30ce9d281a9e42da55f7f4b9ef5bfc6"
+     default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
